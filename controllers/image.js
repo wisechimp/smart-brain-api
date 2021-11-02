b689cfb19132d6aebde7d7938839fb8afa5d8d20
shicks1080 <stuart.hicks@gmail.com>
2021-11-02 13:28:26 +0100

    Adding my Clarifai key
    Plus dotenv for protection

--- controllers/image.js
+++ controllers/image.js
@@ -1,9 +1,12 @@
-const Clarifai = require('clarifai');
+import * as dotenv from "dotenv"
+const Clarifai = require("clarifai")
 
-//You must add your own API key here from Clarifai. 
+dotenv.config()
+
+//You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
- apiKey: 'YOUR API KEY HERE' 
-});
+  apiKey: process.env.CLARIFAI_KEY,
+})
 
 const handleApiCall = (req, res) => {
   app.models
@@ -17,24 +20,25 @@ const handleApiCall = (req, res) => {
     // to:
     // .predict('53e1df302c079b3db8a0a36033ed2d15', req.body.input)
     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
-      res.json(data);
+    .then((data) => {
+      res.json(data)
     })
-    .catch(err => res.status(400).json('unable to work with API'))
+    .catch((err) => res.status(400).json("unable to work with API"))
 }
 
 const handleImage = (req, res, db) => {
-  const { id } = req.body;
-  db('users').where('id', '=', id)
-  .increment('entries', 1)
-  .returning('entries')
-  .then(entries => {
-    res.json(entries[0]);
-  })
-  .catch(err => res.status(400).json('unable to get entries'))
+  const { id } = req.body
+  db("users")
+    .where("id", "=", id)
+    .increment("entries", 1)
+    .returning("entries")
+    .then((entries) => {
+      res.json(entries[0])
+    })
+    .catch((err) => res.status(400).json("unable to get entries"))
 }
 
 module.exports = {
   handleImage,
-  handleApiCall
+  handleApiCall,
 }
