d05a91b39962b90d6355fd3ffbca7c4bd3cc925e
shicks1080 <stuart.hicks@gmail.com>
2022-02-09 07:58:22 +0100

    Adding webtokens and redis to the signin process
    Yeah redis isn't playing nicely at the moment!

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,26 +1,78 @@
-const handleSignin = (db, bcrypt) => (req, res) => {
-  const { email, password } = req.body;
+const jwt = require("jsonwebtoken")
+const redis = require("redis")
+const { promisify } = require("util")
+
+const redisClient = redis.createClient(process.env.REDIS_URI)
+
+redisClient.on("error", () => {
+  console.log("Redis connect error: ", error)
+})
+
+const setAsync = promisify(redisClient.set).bind(redisClient)
+
+const checkUser = (db, bcrypt, req, res) => {
+  const { email, password } = req.body
   if (!email || !password) {
-    return res.status(400).json('incorrect form submission');
+    return Promise.reject("incorrect form submission")
   }
-  db.select('email', 'hash').from('login')
-    .where('email', '=', email)
-    .then(data => {
-      const isValid = bcrypt.compareSync(password, data[0].hash);
+  return db
+    .select("email", "hash")
+    .from("login")
+    .where("email", "=", email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(password, data[0].hash)
       if (isValid) {
-        return db.select('*').from('users')
-          .where('email', '=', email)
-          .then(user => {
-            res.json(user[0])
-          })
-          .catch(err => res.status(400).json('unable to get user'))
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", email)
+          .then((user) => user[0])
+          .catch((err) => Promise.reject("unable to get user"))
       } else {
-        res.status(400).json('wrong credentials')
+        Promise.reject("wrong credentials")
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch((err) => Promise.reject("wrong credentials"))
+}
+
+const getAuthTokenId = () => {
+  console.log("auth ok")
+}
+
+const signToken = (email) => {
+  const jwtPayload = { email }
+  return jwt.sign(jwtPayload, process.env.JWT_SECRET, { expiresIn: "2 days" })
+}
+
+const setToken = async (key, value) => {
+  return await setAsync(key, value)
+}
+
+const createSession = (user) => {
+  // Create the JWT here and return user data
+  const { email, id } = user
+  const token = signToken(email)
+  return setToken(token, id)
+    .then(() => {
+      return { success: "true", userId: id, token }
+    })
+    .catch(console.log)
+}
+
+const handleSigninAuthentication = (db, bcrypt) => (req, res) => {
+  const { authorisation } = req.headers
+  return authorisation
+    ? getAuthTokenId()
+    : checkUser(db, bcrypt, req, res)
+        .then((data) => {
+          return data.id && data.email
+            ? createSession(data)
+            : Promise.reject(data)
+        })
+        .then((session) => res.json(session))
+        .catch((err) => res.status(400).json(err))
 }
 
 module.exports = {
-  handleSignin: handleSignin
+  handleSigninAuthentication: handleSigninAuthentication,
 }
